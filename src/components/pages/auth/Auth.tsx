f4d663cfb10e1087a58c68f37835892dcbf62d47
unknown <timorust1@gmail.comm>
2022-09-27 12:04:51 +0300

    firebase add

--- src/components/pages/auth/Auth.tsx
+++ src/components/pages/auth/Auth.tsx
@@ -1,19 +1,32 @@
-import { Button, ButtonGroup, Grid, TextField } from '@mui/material'
+import { Alert, Button, ButtonGroup, Grid, TextField } from '@mui/material'
 import React, { FC, SyntheticEvent, useState } from 'react'
 import { IUserData } from './types'
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth'
 
 const Auth: FC = () => {
+	const [error, setError] = useState('')
 	const [isRegForm, setIsRegForm] = useState(false)
 	const [userData, setUserData] = useState<IUserData>({
 		email: '',
 		password: '',
 	} as IUserData)
 
-	const handleLogin = (e: SyntheticEvent<HTMLFormElement>) => {
+	const handleLogin = async (e: SyntheticEvent<HTMLFormElement>) => {
 		e.preventDefault()
 
+		const auth = getAuth()
+
 		if (isRegForm) {
-			console.log('register')
+			try {
+				const user = await createUserWithEmailAndPassword(
+					auth,
+					userData.email,
+					userData.password
+				)
+				console.log(user)
+			} catch (error: any) {
+				error.message && setError(error.message)
+			}
 		} else {
 			console.log('auth')
 		}
@@ -26,39 +39,48 @@ const Auth: FC = () => {
 	}
 
 	return (
-		<Grid display='flex' justifyContent='center' alignItems='center'>
-			<form onSubmit={handleLogin}>
-				<TextField
-					type='email'
-					label='Email'
-					variant='outlined'
-					value={userData.email}
-					onChange={(e) => setUserData({ ...userData, email: e.target.value })}
-					sx={{ display: 'block', marginBottom: 3 }}
-					required
-				/>
-				<TextField
-					type='password'
-					label='Password'
-					variant='outlined'
-					value={userData.password}
-					onChange={(e) =>
-						setUserData({ ...userData, password: e.target.value })
-					}
-					sx={{ display: 'block', marginBottom: 3 }}
-					required
-				/>
+		<>
+			{error && (
+				<Alert severity='error' style={{ marginBottom: 20 }}>
+					{error}
+				</Alert>
+			)}
+			<Grid display='flex' justifyContent='center' alignItems='center'>
+				<form onSubmit={handleLogin}>
+					<TextField
+						type='email'
+						label='Email'
+						variant='outlined'
+						value={userData.email}
+						onChange={(e) =>
+							setUserData({ ...userData, email: e.target.value })
+						}
+						sx={{ display: 'block', marginBottom: 3 }}
+						required
+					/>
+					<TextField
+						type='password'
+						label='Password'
+						variant='outlined'
+						value={userData.password}
+						onChange={(e) =>
+							setUserData({ ...userData, password: e.target.value })
+						}
+						sx={{ display: 'block', marginBottom: 3 }}
+						required
+					/>
 
-				<ButtonGroup variant='outlined'>
-					<Button type='submit' onClick={() => setIsRegForm(false)}>
-						Auth
-					</Button>
-					<Button type='submit' onClick={() => setIsRegForm(true)}>
-						Register
-					</Button>
-				</ButtonGroup>
-			</form>
-		</Grid>
+					<ButtonGroup variant='outlined'>
+						<Button type='submit' onClick={() => setIsRegForm(false)}>
+							Auth
+						</Button>
+						<Button type='submit' onClick={() => setIsRegForm(true)}>
+							Register
+						</Button>
+					</ButtonGroup>
+				</form>
+			</Grid>
+		</>
 	)
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,27 @@
-import React from "react";
-import ReactDom from "react-dom";
-import Layout from "./components/layout/Layout";
-import Routes from "./components/routes/Routes";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
+import React, { createContext } from 'react'
+import ReactDom from 'react-dom'
+import Layout from './components/layout/Layout'
+import Routes from './components/routes/Routes'
+import { BrowserRouter } from 'react-router-dom'
+import './index.css'
+import * as firebase from 'firebase/app'
+
+firebase.initializeApp({
+	apiKey: 'AIzaSyDeZnrBAiqacGB7zx-NHVfT6sImmkbsE4g',
+	authDomain: 'contact-app-1fc86.firebaseapp.com',
+	projectId: 'contact-app-1fc86',
+	storageBucket: 'contact-app-1fc86.appspot.com',
+	messagingSenderId: '1071347376540',
+	appId: '1:1071347376540:web:73d9c4077445e12eae2859',
+})
 
 ReactDom.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Layout>
-        <Routes />
-      </Layout>
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+	<React.StrictMode>
+		<BrowserRouter>
+			<Layout>
+				<Routes />
+			</Layout>
+		</BrowserRouter>
+	</React.StrictMode>,
+	document.getElementById('root')
+)
